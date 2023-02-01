6348354cdc6a572d3c87468c5cbe7e5b8cb902cc
Shaco216 <p.griffin.1911@web.de>
2023-02-01 22:35:31 +0100

    Lastname wurde fast fertig gestellt - keine zusatz hrefs mehr

--- LastNameCreator.py
+++ LastNameCreator.py
@@ -5,6 +5,7 @@ class LastNameCreator:
     name_cache = []
     _crawler = Crawler()
     _searcher = HTML_SearchEngine()
+    _result = []
     def __init__(self):
         self._crawler = Crawler("https://de.wiktionary.org/wiki/Verzeichnis:Deutsch/Namen/die_h%C3%A4ufigsten_Nachnamen_Deutschlands")
 
@@ -13,5 +14,15 @@ class LastNameCreator:
         HTMLpage = self._crawler.Generate_HTML_Code_without_searchword()
         self._searcher.set_HTML_Code(HTMLpage)
         self._searcher.choose_Tag(3)
-        self._searcher.search_for("result-name")
-        self._searcher.filter_to_only_text()
+        self._searcher.search_for("/wiki/")
+        #self._searcher.filter_to_only_text()
+        self._searcher.show_current_results()
+        self._result = self._searcher.get_current_results()
+
+    def filter_all_hrefs(self):
+        Result_List = [item for item in self._result if "Verzeichnis" not in str(item)
+                       and "Spezial" not in str(item)
+                       and "Wiktionary" not in str(item)
+                       and "wikimedia" not in str(item)
+                       and "Hilfe:Hinweise" not in str(item)]
+        print(Result_List)
--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from FirstNameCreator import *
+from LastNameCreator import *
 
 
-firstname_Collection = FirstNameCreator()
-firstname_Collection.search_for_limited_amount_of_names(30)
-print(firstname_Collection.name_cache)
+#firstname_Collection = FirstNameCreator()
+#firstname_Collection.search_for_limited_amount_of_names(30)
+#print(firstname_Collection.name_cache)
+
+lastname_collection = LastNameCreator()
+lastname_collection.get_all_Last_Names()
+lastname_collection.filter_all_hrefs()
 
