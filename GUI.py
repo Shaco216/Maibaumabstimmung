3407658d1bf59f8141068f16574571ca15fafb33
Shaco216 <p.griffin.1911@web.de>
2023-04-20 16:06:32 +0200

    Traceback (most recent call last):   File "C:\Users\User\AppData\Local\Programs\Python\Python310\lib\tkinter\__init__.py", line 1921, in __call__     return self.func(*args)   File "C:\Users\User\PycharmProjects\Maibaumabstimmung\GUI.py", line 74, in <lambda>     self.create_point_of_emails(textboxDomain.get("1.0",END),optionmenuDesign.grab_current(),textboxSeperator.get("1.0",END)),statusLabel.config(text=self.change_state_to_warte_auf_passwort())])   File "C:\Users\User\PycharmProjects\Maibaumabstimmung\GUI.py", line 102, in create_point_of_emails     EmailCreator.create_email_address_name(optionforEmailCreator, self.savepointOfNames[0].Get_Namelist()[i],   File "C:\Users\User\PycharmProjects\Maibaumabstimmung\EmailAdressCreator.py", line 47, in create_email_address_name     self._currentEmailaddress = EmailAddress UnboundLocalError: local variable 'EmailAddress' referenced before assignment

--- GUI.py
+++ GUI.py
@@ -14,17 +14,24 @@ from UserAdministration import *
 class GUI_Maibaum:
     firstname_Collection = FirstNameCreator()
     lastname_collection = LastNameCreator()
+
+    #Savepoints
     savepointOfNames = []
+    savepointOfPW = []
+    savepointOfEmail = []
+
     Fenster = tkinter.Tk()
     statustext = "Status: Warte auf Vornamen"
 
     #TODO: https://stackoverflow.com/questions/45441885/how-can-i-create-a-dropdown-menu-from-a-list-in-tkinter
-    options = ["v.nachname"]
-
+    options = ["v.nachname","vorname.nachname","vorname.n"]
+    stringvariable = StringVar(Fenster)
+    stringvariable.set(options[0])
 
     def __init__(self):
         self.Fenster.title('Maibaumabstimmung')
         self.Fenster.geometry('450x320')
+
         #Labels
         statusLabel = Label(master=self.Fenster, text=self.statustext)
         statusLabel.place(x=135,y=5,width=150,height=20)
@@ -38,6 +45,10 @@ class GUI_Maibaum:
         textboxAnzahlNamen.insert(END, "Anzahl Namen")
         textboxAnzahlNamen.place(x=55,y=85,width=150,height=20)
 
+        textboxAnzahlZeichenPasswort = Text(master=self.Fenster, bg="Yellow")
+        textboxAnzahlZeichenPasswort.insert(END,"Anzahl Passwortzeichen")
+        textboxAnzahlZeichenPasswort.place(x=55,y=145, width=150, height=20)
+
         textboxDomain = Text(master=self.Fenster, bg="Yellow")
         textboxDomain.insert(END, "Domain z.b. web.de")
         textboxDomain.place(x=225,y=25,width=150,height=20)
@@ -47,26 +58,76 @@ class GUI_Maibaum:
         textboxSeperator.place(x=225,y=55,width=150,height=20)
 
         # All Buttons
-        buttonNamenSuchen = Button(master=self.Fenster, bg='DeepSkyBlue2', text='Suche Namen', command=lambda: [
+        buttonNamenSuchen = Button(master=self.Fenster, bg='DeepSkyBlue2', text='Suche Namen', command=lambda: [statusLabel.config(text=self.change_state_to_suche_namen()),
             self.firstname_Collection.search_for_limited_amount_of_names(int(textboxAnzahlVornamen.get("1.0", END))),
             self.lastname_collection.get_all_Last_Names(),
             self.lastname_collection.rephrase_umlauts(), statusLabel.config(text=self.change_state_to_warte_auf_namenserstellung())])  # mit command= kann man eine methode aufrufen
         buttonNamenSuchen.place(x=55, y=55, width=150, height=20)
 
         buttonNamenErstellen = Button(master=self.Fenster, bg='DeepSkyBlue2', text="Erstelle Namen",
-                                      command=lambda: self.create_point_of_names(int(textboxAnzahlNamen.get("1.0", END))))
+                                      command=lambda: [statusLabel.config(text=self.change_state_to_erstelle_namensliste()),self.create_point_of_names(int(textboxAnzahlNamen.get("1.0", END))),
+                                                       statusLabel.config(text=self.change_state_to_warte_auf_emailadressen())])
         buttonNamenErstellen.place(x=55, y=115, width=150, height=20)
+
+        buttonEmailsErstellen = Button(master=self.Fenster, bg="DeepSkyBlue2", text="Erstelle Emails",command=lambda:
+                                        [statusLabel.config(text=self.change_state_to_erzeuge_emails()),
+                                         self.create_point_of_emails(textboxDomain.get("1.0",END),optionmenuDesign.grab_current(),textboxSeperator.get("1.0",END)),statusLabel.config(text=self.change_state_to_warte_auf_passwort())])
+        buttonEmailsErstellen.place(x=225, y=115, width=150, height=20)
+        buttonPasswortErstellen = Button(master=self.Fenster,bg='DeepSkyBlue2', text="Erstelle Passwort",command=lambda: [statusLabel.config(text=self.change_state_to_erstelle_passwort()),
+                                                                                                                          ])
+
+        # All Optionmenues
+        optionmenuDesign = OptionMenu(self.Fenster, self.stringvariable, *self.options)
+        optionmenuDesign.place(x=225,y=85,width=150,height=20)
 
         self.Fenster.mainloop()
 
+    #ButtonLogik
     def create_point_of_names(self,Amount):
         NameRandomizer = FullNameRandomizer(self.firstname_Collection.name_cache, self.lastname_collection.name_cache)
         NameRandomizer.Create_Multiple_random_names(Amount)
         self.savepointOfNames.append(NameRandomizer)
-        print(NameRandomizer.Get_Namelist())
+        #print(NameRandomizer.Get_Namelist())
+
+    def create_point_of_pw(self,Amount):
+        pwgen = PWGen()
+
+    def create_point_of_emails(self,domainname,optionOfEmailStructure,seperatorkey):
+        EmailCreator = EmailAdressCreator(self.savepointOfNames[0].Get_Namelist())
+        EmailCreator.set_domain_name(domainname)
+        lengthOfNamelist = len(self.savepointOfNames[0].Get_Namelist())
+        optionforEmailCreator = optionOfEmailStructure
+        seperatorChar = seperatorkey
+        for i in range(lengthOfNamelist):
+            EmailCreator.create_email_address_name(optionforEmailCreator, self.savepointOfNames[0].Get_Namelist()[i],
+                                                   seperatorChar)
+            EmailCreator.create_full_email_with_current_domain_from_currentemail()
+            EmailCreator.add_Current_EMailAddress_to_EmailAddressList()
+        self.savepointOfEmail.append(EmailCreator)
+
+    #Statusänderungen
+    def change_state_to_suche_namen(self):
+        self.statustext="Status: Suche Namen..."
+        return self.statustext
     def change_state_to_warte_auf_namenserstellung(self):
         self.statustext="Status: Warte auf Namenserstellung"
         return self.statustext
+    def change_state_to_erstelle_namensliste(self):
+        self.statustext="Status: Erstelle Namensliste..."
+        return self.statustext
+    def change_state_to_warte_auf_passwort(self):
+        self.statustext="Status: Warte auf Passworterstellung"
+        return self.statustext
+    def change_state_to_erstelle_passwort(self):
+        self.statustext="Status: Erstelle Passwort..."
+    def change_state_to_warte_auf_emailadressen(self):
+        self.statustext="Status: Warte auf Emailerstellung"
+        return self.statustext
+    def change_state_to_erzeuge_emails(self):
+        self.statustext="Status: Erzeuge Emails..."
+        return self.statustext
+
+
 
 
 
